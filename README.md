0f4ad17e64227d6221e1b7b39d6a1eaa7b2b7a45
Atul93 <54507991+Atuls-git@users.noreply.github.com>
2023-12-14 21:03:45 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # TelegramTradingBot
  An Telegram trading bot
+
+## Installation Instructions
+
+### Clone the repo
+```bash
+git clone https://github.com/Atuls-git/TelegramTradingBot.git
+```
+### Move to cloned folder
+```bash
+cd TelegramTradingBot
+```
+### Get permissions
+```bash
+chmod +x *
+```
+### Install dependencies
+```bash
+./setup.sh
+```
+### Run the main file
+```bash
+python3 main.py
+```
+Enjoy :)
---
+++ main.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+import logging
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import os
+import subprocess
+import time
+
+try:
+    from colorama import init, Fore
+except ImportError:
+    print(Fore.Red + "Colorama is not installed. Installing it...")
+    subprocess.run(["pip", "install", "colorama"], check=True)
+    from colorama import init, Fore
+
+# Initialize colorama
+init(autoreset=True)
+
+# Replace these placeholders with your actual API keys and endpoints
+CRYPTO_API_KEY = 'your_crypto_api_key'
+TELEGRAM_BOT_TOKEN = 'your_telegram_bot_token'
+
+# Placeholder for your cryptocurrency trading API
+def buy_token(user_id, amount, currency):
+    # Implement your logic to execute a buy order
+    pass
+
+def start(update: Update, context: CallbackContext) -> None:
+    user_id = update.effective_user.id
+    context.user_data['wallet_connected'] = False
+
+    update.message.reply_text(Fore.GREEN + f"Hello! Welcome to the trading bot. Connect your wallet using /connect_wallet.")
+
+def connect_wallet(update: Update, context: CallbackContext) -> None:
+    user_id = update.effective_user.id
+
+    # Implement your logic to connect the user's wallet
+    # For simplicity, let's assume the wallet is connected successfully
+    context.user_data['wallet_connected'] = True
+
+    update.message.reply_text(Fore.CYAN + "Wallet connected successfully!")
+
+def buy(update: Update, context: CallbackContext) -> None:
+    user_id = update.effective_user.id
+
+    if not context.user_data.get('wallet_connected', False):
+        update.message.reply_text(Fore.LIGHTRED_EX + "Please connect your wallet first using /connect_wallet.")
+        return
+
+    # Extracting the command parameters (amount and currency)
+    try:
+        command_parts = context.args[0].split('_')
+        amount = float(command_parts[0])
+        currency = command_parts[1].upper()
+    except (IndexError, ValueError):
+        update.message.reply_text(Fore.LIGHTRED_EX + "Invalid command. Use /buy <amount>_<currency> format, e.g., /buy 1_ETH.")
+        return
+
+    # Placeholder for checking if the currency is supported
+    supported_currencies = ['ETH', 'USDT', 'USDC', 'PEPE']
+    if currency not in supported_currencies:
+        update.message.reply_text(Fore.LIGHTRED_EX + f"Unsupported currency: {currency}. Supported currencies: {', '.join(supported_currencies)}")
+        return
+
+    # Placeholder for executing the buy order
+    buy_token(user_id, amount, currency)
+
+    update.message.reply_text(Fore.LIGHTMAGENTA_EX + f"Buy order executed: {amount} $FREE bought with {amount} {currency}.")
+
+def main():
+    # Set up logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    # Create the Updater and pass it your bot's token
+    updater = Updater(TELEGRAM_BOT_TOKEN)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Register command handlers
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("connect_wallet", connect_wallet))
+    dp.add_handler(CommandHandler("buy", buy))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you send a signal to stop it
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
